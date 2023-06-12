3cbfb91c1827566b5f88ffbe8d447aec789a2937
Isaac Tierney <itiereny100@gmail.com>
2023-06-12 16:03:15 -0400

    Application Db Context

---
+++ ElevenFiftyFlights.Data/ApplicationDbContext.cs
@@ -0,0 +1,9 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace ElevenFiftyFlights.Data;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options) {  }
+}
