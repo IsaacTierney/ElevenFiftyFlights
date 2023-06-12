450e3cedf55ec90c5404962583cc812c2f6d6c9e
Brianne W <brianne4god@gmail.com>
2023-06-12 16:05:01 -0400

    Dbcontext Options

--- ElevenFiftyFlights.WebAPI/Program.cs
+++ ElevenFiftyFlights.WebAPI/Program.cs
@@ -1,4 +1,11 @@
+using ElevenFiftyFlights.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// Add connection string and DbContext setup
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(connectionString));
 
 // Add services to the container.
 
