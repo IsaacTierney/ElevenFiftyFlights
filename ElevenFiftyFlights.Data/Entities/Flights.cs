71b44f383beccf4e0b0380666b61e456f1dbf0cb
Edwin Torres <me@edwintorres.dev>
2023-06-13 15:33:51 -0400

    Created files for Airports and Flights

---
+++ ElevenFiftyFlights.Data/Entities/Flights.cs
@@ -0,0 +1,25 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ElevenFiftyFlights.Data.Entities;
+
+public class FlightEntity
+{
+	[Key]
+	public int Id { get; set; }
+
+	[ForeignKey("Airlines.Id")]
+	public int AirlineId { get; set; }
+
+	[ForeignKey("Airports.Id")]
+	public int OriginId { get; set; }
+
+	[ForeignKey("Airports.Id")]
+	public int DestinationId { get; set; }
+
+	public DateTime DepartureTime { get; set; }
+
+	public float TicketPrice { get; set; }
+
+	public string Gate { get; set; }
+}
