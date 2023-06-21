caf91da2090726d0cafa3bfe0c4cba23b3f4a32f
Edwin Torres <me@edwintorres.dev>
2023-06-21 15:15:31 -0400

    Created Register Service Method

--- ElevenFiftyFlights.Services/AirportService.cs
+++ ElevenFiftyFlights.Services/AirportService.cs
@@ -1,6 +1,31 @@
+using ElevenFiftyFlights.Data;
+using ElevenFiftyFlights.Data.Entities;
+using ElevenFiftyFlights.Models.Airport;
+
 namespace ElevenFiftyFlights.Services.Airport;
 
 public class AirportService : IAirportService
 {
-	
+	private readonly ApplicationDbContext _context;
+	public AirportService(ApplicationDbContext context)
+	{
+		_context = context;
+	}
+
+	public async Task<bool> RegisterAirportAsync(AirportRegister model)
+	{
+		AirportEntity entity = new()
+		{
+			Country = model.Country,
+			City = model.City,
+			State = model.State,
+			Name = model.Name,
+			Code = model.Code
+		};
+
+		_context.Airport.Add(entity);
+		int numberOfChanges = await _context.SaveChangesAsync();
+
+		return numberOfChanges == 1;
+	}
 }
--- ElevenFiftyFlights.Services/FlightService.cs
+++ ElevenFiftyFlights.Services/FlightService.cs
@@ -1,6 +1,32 @@
+using ElevenFiftyFlights.Data;
+using ElevenFiftyFlights.Data.Entities;
+using ElevenFiftyFlights.Models.Flight;
+
 namespace ElevenFiftyFlights.Services.Flight;
 
 public class FlightService : IFlightService
 {
-	
+	private readonly ApplicationDbContext _context;
+	public FlightService(ApplicationDbContext context)
+	{
+		_context = context;
+	}
+
+	public async Task<bool> RegisterFlightAsync(FlightRegister model)
+	{
+		FlightEntity entity = new()
+		{
+			AirlineId = model.AirlineId,
+			OriginId = model.OriginId,
+			DestinationId = model.DestinationId,
+			DepartureTime = model.DepartureTime,
+			TicketPrice = model.TicketPrice,
+			Gate = model.Gate
+		};
+
+		_context.Flight.Add(entity);
+		int numberOfChanges = await _context.SaveChangesAsync();
+
+		return numberOfChanges == 1;
+	}
 }
