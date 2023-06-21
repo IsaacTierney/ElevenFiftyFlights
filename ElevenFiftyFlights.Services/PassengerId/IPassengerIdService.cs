ee7b201577cbe69a1bda4d98459c3dbd33125d2f
Brianne W <brianne4god@gmail.com>
2023-06-21 18:26:54 -0400

    Updated methods of Delete and Update Passenger

--- ElevenFiftyFlights.Services/PassengerId/IPassengerIdService.cs
+++ ElevenFiftyFlights.Services/PassengerId/IPassengerIdService.cs
@@ -8,7 +8,9 @@ public interface IPassengerIdService
     //Declaration for method implementations in the service 
     //Data model as return types     Method Name             (paramaters)
     Task<PassengerIdResponse?> GetPassengerIdAsync(PassengerIdResponse model);
-    Task<PassengerIdDetail?> BookpassengerAsync(PassengerIdBooking model);
+    Task<PassengerIdDetail?> BookPassengerAsync(PassengerIdBooking model);
     Task<PassengerIdDetail> GetPassengerDetailById(int Id);
-    Task<FlightDetail> GetAllFlightsByIdAsync(int Id);
+    Task<IEnumerable<PassengerFlightListItem>> GetAllFlightsAsync(int Id);
+    Task<bool> UpdatePassengerAsync(PassengerIdBooking request, int userId);
+    Task<bool> DeletePassengerByIdAsync(int passengerId);
 }
