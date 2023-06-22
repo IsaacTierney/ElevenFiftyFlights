5892ef2d7f59f1614bb3a40a33dd4c6412219fcc
Isaac Tierney <itiereny100@gmail.com>
2023-06-22 10:45:08 -0400

    User Updates

--- ElevenFiftyFlights.WebAPI/Controllers/UserController.cs
+++ ElevenFiftyFlights.WebAPI/Controllers/UserController.cs
@@ -1,6 +1,7 @@
 using ElevenFiftyFlights.Services.User;
 using Microsoft.AspNetCore.Mvc;
 using ElevenFiftyFlights.Models.User;
+using Microsoft.AspNetCore.Authorization;
 
 namespace ElevenFiftyFlights.WebAPI.Controllers
 {
@@ -12,6 +13,23 @@ namespace ElevenFiftyFlights.WebAPI.Controllers
         public UserController(IUserService userService)
         {
             _userService = userService;
+        }
+
+        [HttpPost("Register")]
+        public async Task<IActionResult> RegisterUser([FromBody] UserRegister model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var registerResult = await _userService.RegisterUserAsync(model);
+            if (registerResult)
+            {
+                return Ok("User was registered.");
+            }
+       
+            return BadRequest("Could not registere.");
         }
     }
 }
